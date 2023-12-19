ebd7f2699a72dc54527748427b314a569c7d2baa
Samsonov Dmitriy <samsonov567@gmail.com>
2023-12-19 11:43:33 +0300

    Добавить карточки на страницу, реализовать функцию удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content
+const cardContainer = document.querySelector('.places')
+const cardList = cardContainer.querySelector('.places__list')
 
-// @todo: Функция создания карточки
+function addCard(cardTitle, imageLink, deleteFunction) {
+	const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true)
+	cardElement.querySelector('.card__title').textContent = cardTitle
+	cardElement.querySelector('.card__image').src = imageLink
+	cardElement.querySelector('.card__image').alt = cardTitle
+	const deleteButton = cardElement.querySelector('.card__delete-button')
+	deleteButton.addEventListener('click', deleteFunction)
+	return cardElement
+}
 
-// @todo: Функция удаления карточки
+function deleteCard(evt){
+	evt.target.closest('.places__item').remove()
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach( element => {
+	cardList.append(addCard(element.name, element.link, deleteCard))
+})
